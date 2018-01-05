4fd1c7f8f999f7d3866f445d6523b25730e2e0d0
Administrator <Administrator@ZHY-20170622QGM>
2018-01-05 15:51:57 +0800

    数组最大长度测试，MD5加密工具

---
+++ practice/src/test/java/com/gzq/ArrayTest.java
@@ -0,0 +1,12 @@
+package com.gzq;
+
+import java.io.ByteArrayOutputStream;
+
+public class ArrayTest {
+	
+	public static void main(String[] args) {
+		long year = Long.MAX_VALUE / 1000 / 60 / 60 /24 /365;
+		System.out.println(year);
+	}
+
+}
