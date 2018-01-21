60b26f1c3053539349001d72d1016a599620484d
GeYi <825468477@qq.com>
2018-01-21 14:47:37 +0800

    线程等待与唤醒的案例

---
+++ practice/src/test/java/com/gzq/thread/basic/suspend/WaitNotify1.java
@@ -0,0 +1,35 @@
+package com.gzq.thread.basic.suspend;
+
+/**
+ * 验证wait、notify方法都需要synchronized监视当前的方法调用的对象。
+ * @author GeYi
+ *
+ */
+public class WaitNotify1 {
+    public static void main(String[] args) {
+        Object object = new Object();
+        Object object2 = new Object();
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    synchronized (object) {
+                         System.out.println("===线程开始===");
+                        try {
+                            object.wait();
+                            System.out.println("--接着上次执行---");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+        thread.start();
+        System.out.println("main thread end");
+//        synchronized (object2) {
+            object.notifyAll();
+            System.out.println("唤醒线程");
+//        }
+    }
+}
---
+++ practice/src/test/java/com/gzq/thread/basic/suspend/WaitNotify2.java
@@ -0,0 +1,46 @@
+package com.gzq.thread.basic.suspend;
+
+/**
+ * 验证notify方法的随机唤醒。
+ * @author GeYi
+ *
+ */
+public class WaitNotify2 {
+    public static void main(String[] args) throws InterruptedException {
+        Object object = new Object();
+        for (int i = 0; i < 30; i++) {
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    synchronized (object) {
+                        System.out.println(Thread.currentThread().getName());
+                        try {
+                            object.wait();
+                            System.out.println(
+                                    Thread.currentThread().getName() + "--接着上次执行---");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            };
+            thread.setName("线程i " + i);
+            thread.start();
+            Thread.sleep(10);// 保证各个线程都按顺序等待在object的wait区。
+        }
+        System.out.println("main thread end");
+        Thread.sleep(100);
+        for (int i = 0; i < 20; i++) {
+            synchronized (object) {
+                object.notify();
+                System.out.println("notify唤醒线程");
+            }
+        }
+
+//        synchronized (object) {
+//            object.notifyAll();
+//            System.out.println("notifyAll唤醒线程");
+//        }
+    }
+    
+}
